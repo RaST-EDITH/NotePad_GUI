88a53a2f21edcbad6ab298bb68fbf44463005cd2
raghvsinghtomar1975@gmail.com <raghvsinghtomar1975@gmail.com>
2024-01-08 23:00:37 +0530

    Basic Library added

---
+++ app.py
@@ -0,0 +1,9 @@
+# Library and Modules used 
+import os                                                    # pip install os ( In case if not present )
+import numpy as np                                           # pip install numpy ( In case if not present )
+import pandas as pd                                          # pip install pandas==1.4.3
+import openpyxl as oxl                                       # pip install openpyxl==3.0.10
+from tkinter import *                                        # pip install tkinter==8.6
+import customtkinter as ctk                                  # pip install customtkinter==4.6.3
+from PIL import Image ,ImageTk                               # pip install pillow==9.3.0
+from tkinter.messagebox import showerror, showinfo
